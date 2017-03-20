44e321b2d59e1ae8ffddc8fe5b344f00745bcb3a
vagrant <vagrant@homestead>
2017-03-20 20:43:06 +0000

    rough out download

---
+++ src/AppBundle/Command/DownloadCommand.php
@@ -0,0 +1,41 @@
+<?php
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+use AtlasGrove\Utils as Utils;
+use AtlasGrove\TigerlineDownload as TigerlineDownload;
+
+// php bin/console atlasgrove:download
+
+class DownloadCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('atlasgrove:download');
+        $this->setDescription('downloads Tiger/Line shapefiles to cache (parms: year, state)');
+        
+        $this->addArgument('year', InputArgument::REQUIRED);
+        $this->addArgument('state', InputArgument::REQUIRED);
+        
+        $this->addOption('path', 'p', InputOption::VALUE_REQUIRED, '', getcwd());
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $util=new Utils($this->getContainer(),$io);
+        $tigerline=new TigerlineDownload($this->getContainer(),$io);
+        
+        $year = $input->getArgument('year')?:"";
+        $state = $input->getArgument('state')?:"";
+        
+        $tigerline->download($year,$state);
+    }
+}
