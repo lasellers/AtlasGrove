7d660e3191f81bc80eb22dce529ff46ad51bf92c
Lewis A. Sellers <lasellers@gmail.com>
2017-03-30 22:47:26 -0400

    Change caching from using simple files to the symfony 3 caching mechanism

--- src/AppBundle/Command/CacheCommand.php
+++ src/AppBundle/Command/CacheCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -8,7 +9,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-use AtlasGrove\Tigerline as Tigerline;
 use AtlasGrove\TigerlineCache as TigerlineCache;
 
 // php bin/console atlasgrove:cache
@@ -19,81 +19,75 @@ class CacheCommand extends ContainerAwareCommand
     {
         $this->setName('atlasgrove:cache');
         $this->setDescription('lists AtlasGrove cached tigerline files');
-        
-        $this->addArgument('id', InputArgument::OPTIONAL,"id");
-        
-        $this->addOption('force', null, InputOption::VALUE_NONE,'If set, uses force');
-        
-        $this->addOption('states-list', null, InputOption::VALUE_NONE,'If set, uses ..');
-        $this->addOption('counties-list', null, InputOption::VALUE_NONE,'If set, uses ..');
-        
-        $this->addOption('states', null, InputOption::VALUE_NONE,'If set, uses ..');
-        $this->addOption('counties', null, InputOption::VALUE_NONE,'If set, uses ..');
-        
-        $this->addOption('all', null, InputOption::VALUE_NONE,'If set, uses ..');
-        
+
+        $this->addArgument('id', InputArgument::OPTIONAL, "id");
+
+        $this->addOption('force', null, InputOption::VALUE_NONE, 'If set, uses force');
+
+        $this->addOption('states-list', null, InputOption::VALUE_NONE, 'If set, uses ..');
+        $this->addOption('counties-list', null, InputOption::VALUE_NONE, 'If set, uses ..');
+
+        $this->addOption('states', null, InputOption::VALUE_NONE, 'If set, uses ..');
+        $this->addOption('counties', null, InputOption::VALUE_NONE, 'If set, uses ..');
+
+        $this->addOption('all', null, InputOption::VALUE_NONE, 'If set, uses ..');
+
         //$this->addOption('path', 'p', InputOption::VALUE_REQUIRED, '', getcwd());
         $this->addOption('path', null, InputOption::VALUE_NONE, 'Optional output path');
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        
-        $tigerline=new TigerlineCache($this->getContainer(),$io);
-        
-        $id = $input->getArgument('id')?:"";
+
+        $tigerline = new TigerlineCache($this->getContainer(), $io);
+
+        $id = $input->getArgument('id') ?: "";
         $io->note("Id=$id");
-        
-        $force = $input->getOption('force',false)?true:false;
-        $io->note('Force '.($force?"YES":"NO"));
-        
-        if( $input->getOption('states-list') || $input->getOption('all') )
-        {
+
+        $force = $input->getOption('force') ? true : false;
+        $io->note('Force ' . ($force ? "YES" : "NO"));
+
+        if ($input->getOption('states-list') || $input->getOption('all')) {
             $io->title('Cache States List');
-            $files=$tigerline->cacheStatesList();
-            
-            foreach($files as &$file)
-            {
-                $file=explode("\t",$file);
+            $files = $tigerline->cacheStatesList();
+
+            foreach ($files as &$file) {
+                $file = explode("\t", $file);
             }
-            $io->table(['id','folder','name'],$files);
+            $io->table(['id', 'folder', 'name'], $files);
         }
-        
-        if( $input->getOption('states') || $input->getOption('all') )
-        {
+
+        if ($input->getOption('states') || $input->getOption('all')) {
             $io->title('Cache States Shapes');
-            $files=$tigerline->cacheStatesList();
-            
-            $records=$tigerline->cacheShapes($files,$force);
+            $files = $tigerline->cacheStatesList();
+
+            $tigerline->cacheShapes($files);
         }
-        
-        if( $input->getOption('counties-list') || $input->getOption('all') )
-        {
+
+        if ($input->getOption('counties-list') || $input->getOption('all')) {
             $io->title('Cache Counties List');
-            $files=$tigerline->cacheCountiesList();
-            
-            foreach($files as &$file)
-            {
-                $file=explode("\t",$file);
+            $files = $tigerline->cacheCountiesList();
+
+            foreach ($files as &$file) {
+                $file = explode("\t", $file);
             }
-            $io->table(['id','county','full'],$files);
+            $io->table(['id', 'county', 'full'], $files);
         }
-        
-        if( $input->getOption('counties') || $input->getOption('all') )
-        {
+
+        if ($input->getOption('counties') || $input->getOption('all')) {
             $io->title('Cache Counties Shapes');
-            $files=$tigerline->cacheCountiesList();
-            
-            $records=$tigerline->cacheShapes($files,$force);
+            $files = $tigerline->cacheCountiesList();
+
+            $tigerline->cacheShapes($files, $force);
         }
-        
-         if($id>0) {
+
+        if ($id > 0) {
             $io->title("Cache Id {$id}");
-            
-            $records=$tigerline->cacheShape($id,$force);
+
+           $tigerline->cacheShape($id, $force);
             //  $io->table(['id','county','full'],$records);
         }
-        
+
     }
 }
